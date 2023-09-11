a54f316be07754fc3fa02c46e40e6d9a9fe077c0
snaidy giraldo <snaidy98g@gmail.com>
2023-09-11 15:18:28 -0600

    :tada: add changes to login structure

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "../pages/login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Registro from "../pages/registro/Registro";
+import LoginVendedor from "../pages/loginVendedor/LoginVendedor";
 
 
 const Router = () => {
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/"> 
               <Route index element={<Login />} />
               <Route path="registro" element={<Registro/>} />
+              <Route path="loginvendedor" element={<LoginVendedor/>} />
             </Route>
       </Routes>
     </BrowserRouter>
