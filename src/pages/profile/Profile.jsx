240064eb9b42c5a2e9a6ffc7657c0284d5edc5dc
stefannyaonofre <sonofreg@uqvirtual.edu.co>
2023-09-12 17:06:56 -0500

    responsive profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,28 +5,33 @@ import "./profile.scss";
 import user from "/test.jfif";
 import arrowB from "/arrow-right.svg";
 import NavMobile from "../../components/nav-mobile/NavMobile";
-const Profile = () => {
+import regalo from "/icons/gift.svg";
+import calendar from "/icons/calendar-color.svg";
+import language from "/icons/language.svg";
+import chat from "/icons/chat-conversation.svg";
+import heart from "/icons/heart-pink.svg";
 
+const Profile = () => {
   const navigate = useNavigate();
   const [widthMovile, setWidthMovile] = useState();
 
   useEffect(() => {
-    handleResize()
+    handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("resize", handleResize);
-    }
-  },[])
+    };
+  }, []);
 
   const handleResize = () => {
     const width = window.innerWidth;
-    if(width <= 768) {
-      setWidthMovile(true)
-    }else{
-      setWidthMovile(false)
+    if (width <= 768) {
+      setWidthMovile(true);
+    } else {
+      setWidthMovile(false);
     }
-  }
+  };
 
   const handleProfileEdit = () => {
     navigate("/profileEdit");
@@ -34,64 +39,93 @@ const Profile = () => {
 
   return (
     <>
-    {
-      widthMovile ? 
-      (<main className="profile">
-      <div className="profile-up">
-        <img src={arrowBack} alt="ArrowBack" onClick={() => navigate(-1)} />
-        <h2>Perfil</h2>
-      </div>
+      {widthMovile ? (
+        <main className="profile">
+          <div className="profile-up">
+            <img src={arrowBack} alt="ArrowBack" onClick={() => navigate(-1)} />
+            <h2>Perfil</h2>
+          </div>
 
-      <div className="profile-info">
-        <figure className="photo">
-          <img src={user} alt="Photo" />
-        </figure>
+          <div className="profile-info">
+            <figure className="photo">
+              <img src={user} alt="Photo" />
+            </figure>
 
-        <div className="date">
-          <h3>Anna</h3>
-          <span
-          className="view"
-          onClick={handleProfileEdit}
-          >Ver perfil</span>
-        </div>
-      </div>
+            <div className="date">
+              <h3>Anna</h3>
+              <span className="view" onClick={handleProfileEdit}>
+                Ver perfil
+              </span>
+            </div>
+          </div>
 
-      <section className="profile-section">
-        <hr className="button-divider" />
-        <div className="order-section">
-          <h3>Ordenes</h3>
-          <figure className="arrow-right">
-            <img src={arrowB} alt="Arrow right" />
-          </figure>
-        </div>
+          <section className="profile-section">
+            <hr className="button-divider" />
+            <div className="order-section">
+              <h3>Ordenes</h3>
+              <figure className="arrow-right">
+                <img src={arrowB} alt="Arrow right" />
+              </figure>
+            </div>
 
-        <hr className="button-divider" />
-        <div className="order-section">
-          <h3>Mis eventos</h3>
-          <figure className="arrow-right">
-            <img src={arrowB} alt="Arrow right" />
-          </figure>
-        </div>
+            <hr className="button-divider" />
+            <div className="order-section">
+              <h3>Mis eventos</h3>
+              <figure className="arrow-right">
+                <img src={arrowB} alt="Arrow right" />
+              </figure>
+            </div>
 
-        <hr className="button-divider" />
-        <div className="order-section">
-          <h3>Español</h3>
-          <figure className="arrow-right">
-            <img src={arrowB} alt="Arrow right" />
-          </figure>
-        </div>
-        <hr className="button-divider" />
-      </section>
+            <hr className="button-divider" />
+            <div className="order-section">
+              <h3>Español</h3>
+              <figure className="arrow-right">
+                <img src={arrowB} alt="Arrow right" />
+              </figure>
+            </div>
+            <hr className="button-divider" />
+          </section>
 
-      <NavMobile/>
-    </main>
-
-      ) : (
-        <main>
-          <div>hola</div>
+          <NavMobile />
         </main>
-      )
-    }
+      ) : (
+        <main className="main-profile-desktop">
+          <h2>Perfil</h2>
+          <div>
+            <div className="main-profile-desktop__info">
+              <figure className="main-profile-desktop__info-photo">
+                <img src={user} alt="Photo" />
+              </figure>
+              <div className="main-profile-desktop__info-user">
+                <h3>Anna</h3>
+                <span onClick={handleProfileEdit}>Ver perfil</span>
+              </div>
+            </div>
+          </div>
+          <section className="main-profile-desktop__filter">
+            <div className="main-profile-desktop__filter__card">
+              <img src={regalo} />
+              <span>Ordenes</span>
+            </div>
+            <div className="main-profile-desktop__filter__card">
+              <img src={calendar} />
+              <span>Mis eventos</span>
+            </div>
+            <div className="main-profile-desktop__filter__card">
+              <img src={language} />
+              <span>Español</span>
+            </div>
+            <div className="main-profile-desktop__filter__card">
+              <img src={chat} />
+              <span>Mensajes</span>
+            </div>
+            <div className="main-profile-desktop__filter__card">
+              <img src={heart} />
+              <span>Favoritos</span>
+            </div>
+          </section>
+        </main>
+      )}
     </>
   );
 };
--- src/pages/profileEdit/ProfileEdit.jsx
+++ src/pages/profileEdit/ProfileEdit.jsx
@@ -1,64 +1,109 @@
-import React from 'react'
-import './profileEdit.scss'
+import React, { useEffect, useState } from "react";
+import "./profileEdit.scss";
 import arrowBack from "/arrowback.svg";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import user from "/test.jfif";
 const ProfileEdit = () => {
   const navigate = useNavigate();
+  const [widthMovile, setWidthMovile] = useState();
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleResize = () => {
+    const width = window.innerWidth;
+    if (width <= 768) {
+      setWidthMovile(true);
+    } else {
+      setWidthMovile(false);
+    }
+  };
+
   return (
-    <main className='profileEdit'>
-      <div className="profileEdit-up">
-        <img src={arrowBack} alt="ArrowBack" onClick={() => navigate(-1)} />
-        <h2>Perfil</h2>
-      </div>
-     <div className='content-user'>
-     <figure className="photo-user">
-          <img src={user} alt="Photo" />
-        </figure>
-     </div>
-        
-
-        <section className='edit-section'>
-        <div className="data">
-          <div className='text'>
-          <span>Tú nombre</span>
-          <h3>Anna</h3>
+    <>
+      {widthMovile ? (
+        <main className="profileEdit">
+          <div className="profileEdit-up">
+            <img src={arrowBack} alt="ArrowBack" onClick={() => navigate(-1)} />
+            <h2>Perfil</h2>
           </div>
-        </div>
-        <hr className="button-divider" />
-
-        <div className="data">
-          <div className='text'>
-          <span>Tú correo</span>
-          <h3>ann@gmail.com</h3>
+          <div className="content-user">
+            <figure className="photo-user">
+              <img src={user} alt="Photo" />
+            </figure>
           </div>
-        </div>
-        <hr className="button-divider" />
 
-        <div className="data">
-          <div className='text'>
-          <span>Tú fecha de nacimiento</span>
-          <h3>22.12.1999</h3>
-          </div>
-        </div>
-        <hr className="button-divider" />
-        <div className="data">
-          <div className='text'>
-          <span>Tú teléfono</span>
-          <h3>+57 3102706787</h3>
-          </div>
-        </div>
-        <hr className="button-divider" />
-        </section>
-        <div className='content-button'>
-        <button className='button-save'>
-          Guardar
-        </button>
-        <span className='close-sesion'>Cerrar sesión</span>
-        </div>
-        
-    </main>
-  )
-}
+          <section className="edit-section">
+            <div className="data">
+              <div className="text">
+                <span>Tú nombre</span>
+                <h3>Anna</h3>
+              </div>
+            </div>
+            <hr className="button-divider" />
 
-export default ProfileEdit
+            <div className="data">
+              <div className="text">
+                <span>Tú correo</span>
+                <h3>ann@gmail.com</h3>
+              </div>
+            </div>
+            <hr className="button-divider" />
+
+            <div className="data">
+              <div className="text">
+                <span>Tú fecha de nacimiento</span>
+                <h3>22.12.1999</h3>
+              </div>
+            </div>
+            <hr className="button-divider" />
+            <div className="data">
+              <div className="text">
+                <span>Tú teléfono</span>
+                <h3>+57 3102706787</h3>
+              </div>
+            </div>
+            <hr className="button-divider" />
+          </section>
+          <div className="content-button">
+            <button className="button-save">Guardar</button>
+            <span className="close-sesion">Cerrar sesión</span>
+          </div>
+        </main>
+      ) : (
+        <main className="profileEdit-desktop">
+          <section className="profileEdit-desktop__card">
+            <h2>Información del Perfil</h2>
+            <figure className="profileEdit-desktop__card__figure">
+              <img src={user} alt="Photo" />
+            </figure>
+            <div className="profileEdit-desktop__card__info">
+              <label>Tu nombre</label>
+              <input type="text" />
+              <label>Tu correo</label>
+              <input type="text" />
+              <label>Tu teléfono</label>
+              <input type="text" />
+              <label>Tu fecha de nacimiento</label>
+              <input type="text" />
+            </div>
+            <div className="profileEdit-desktop__card__button">
+              <button>Guardar</button>
+            </div>
+          </section>
+          <div className="profileEdit-desktop__cerrar">
+            <span>Cerrar sesión</span>
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default ProfileEdit;
